ab342a869422e12a192921110672010b3d9a32bc
Piotr Smialkowski <p.smialkowski@gmail.com>
2016-12-23 11:50:39 +0100

    first commit

---
+++ src/http.js
@@ -0,0 +1,26 @@
+import IConnection from './base';
+
+export class IHttpConnection{
+    constructor(){
+
+    }
+    get(){
+
+    }
+
+    post(){
+
+    }
+
+    delete(){
+
+    }
+
+    options(){
+
+    }
+
+    put(){
+
+    }
+}
---
+++ src/socket.js
@@ -0,0 +1,30 @@
+import IConnection from './base';
+
+
+export class ISocketConnection {
+    constructor(){
+
+    }
+
+    open(){
+    }
+
+    close(){
+
+    }
+
+    call(topic, args){
+
+    }
+
+    register(topic, handler){
+
+    }
+
+    publish(topic, args){
+
+    }
+    subscribe(topic, handler){
+
+    }
+}
